66b42d7f4359b73c4e1eba78d71ab4d05539b5f6
Vitório Eugenio Settin Wingert <vitoriowingert@hotmail.com>
2021-07-06 14:14:35 -0300

    Initial commit
    NodeJS project builded on NLW Together

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,49 @@
+import { getCustomRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    // Verificar se email existe
+    const user = await usersRepositories.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    // verificar se senha está correta
+    // 123456 / 783645734-sdhfhsdf7762374234234
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    // Gerar token
+    const token = sign(
+      {
+        email: user.email,
+      },
+      "44e42e8e3e401091987ef40cfc11358f",
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
